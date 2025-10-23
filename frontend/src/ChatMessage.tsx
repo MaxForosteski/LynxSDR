d3d5d24d23a7948a258fb26054556edab694d47c
Marcos Max Forosteski da Silva <marcos.silva@neomind.com.br>
2025-10-23 04:03:15 -0300

    fix: fix font size for 2xl viewport

--- frontend/src/ChatMessage.tsx
+++ frontend/src/ChatMessage.tsx
@@ -6,7 +6,7 @@ export type ChatMessageProps = {
 export default function ChatMessage({ type, content }: ChatMessageProps) {
     return (
         <div className={"flex w-full " + (type === "user" ? "justify-end" : "justify-start")}>
-            <div className={"p-3 w-fit flex align-center rounded-lg m-3 text-sm 2xl:text-2xl " + (type === "user" ? "bg-black text-white ml-8 max-w-[85%] md:max-w-[65%] xl:max-w-[53%] 2xl:max-w-[45%]" : "bg-gray-200 text-black mr-8 max-w-[95%] 2xl:max-w-[90%]")}>
+            <div className={"p-3 w-fit flex align-center rounded-lg m-3 text-sm 2xl:text-lg " + (type === "user" ? "bg-black text-white ml-8 max-w-[85%] md:max-w-[65%] xl:max-w-[53%] 2xl:max-w-[45%]" : "bg-gray-200 text-black mr-8 max-w-[95%] 2xl:max-w-[90%]")}>
                 <p>{content}</p>
             </div>
         </div>
